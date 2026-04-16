b16468cb27f5e161e27aca346436db45139cf2fe
Rocky0412 <rockytapader4g@gmail.com>
2026-04-17 02:47:28 +0530

    database setup done

---
+++ src/main/java/org/example/uberreviewservice/UberReviewServiceApplication.java
@@ -0,0 +1,14 @@
+package org.example.uberreviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class UberReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberReviewServiceApplication.class, args);
+	}
+
+}
